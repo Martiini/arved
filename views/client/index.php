1a32634282636e1272f3d608d438e72a95a33669
Martin Elias <tutanhamon13@live.com>
2015-05-25 15:52:43 +0300

    No clients found view

--- views/client/index.php
+++ views/client/index.php
@@ -2,6 +2,8 @@
 
 use yii\widgets\LinkPager;
 
+$url = new \yii\helpers\Url();
+
 ?>
 <div class="container well">
     <h1>Client list</h1>
@@ -9,7 +11,7 @@ use yii\widgets\LinkPager;
     <br />
     <br />
 
-    <?php if(count($clients)): ?>
+    <?php if($clients): ?>
         <table class="table table-responsive table-striped">
             <tr>
                 <th>First name</th>
@@ -34,6 +36,8 @@ use yii\widgets\LinkPager;
 
         <?= LinkPager::widget(['pagination' => $pagination]) ?>
     <?php else: ?>
-    <div class="alert alert-info">No clients found</div>
+    <div class="alert alert-info">No clients found <br /> <br />
+        <a href="<?= $url->to('client/create') ?>" class="btn btn-primary">Create client</a>
+    </div>
     <?php endif; ?>
 </div>
