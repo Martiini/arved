6203676e2bb7cf7de8e5f610645b1466fc87f4e0
Martin Elias <tutanhamon13@live.com>
2015-05-25 17:06:39 +0300

    Invoice preview / download

--- web/js/invoice.js
+++ web/js/invoice.js
@@ -1,6 +1,5 @@
-var doc = new jsPDF();
-
 function updatePreview(download) {
+    var doc = new jsPDF();
 
     doc.setFontSize(20);
     doc.text(35, 25, $('#inv_title').text());
@@ -8,14 +7,16 @@ function updatePreview(download) {
     doc.setFontSize(12);
     doc.text(35, 34, 'Client: ' + $('#inv_client').text());
 
-    var urlString = doc.output('dataurlstring');
-
-    $('#pdf_preview').attr({
-        src: urlString
-    });
-
     if (download) {
         doc.save('invoice.pdf');
+    } else {
+
+        var urlString = doc.output('dataurlstring');
+
+        $('#pdf_preview').attr({
+            src: urlString
+        });
+
     }
 
 }
